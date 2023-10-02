8c2cdb55e40f369ac2e985ce1e786de2eb0f71bc
Cameron Little <cameron@remitly.com>
2023-10-02 10:03:34 -0600

    Verify CSP style issue

--- examples/vite-ts/src/main.tsx
+++ examples/vite-ts/src/main.tsx
@@ -1,12 +1,21 @@
-import { ChakraProvider } from "@chakra-ui/react"
+import { ChakraProvider, extendTheme } from "@chakra-ui/react"
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
+import createCache from "@emotion/cache"
+import { CacheProvider } from "@emotion/react"
+
+const cache = createCache({
+  key: "chakra-vite-test",
+  nonce: "exampleexample",
+})
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <CacheProvider value={cache}>
+      <ChakraProvider theme={extendTheme({})}>
+        <App />
+      </ChakraProvider>
+    </CacheProvider>
   </React.StrictMode>,
 )
--- examples/vite-ts/vite.config.ts
+++ examples/vite-ts/vite.config.ts
@@ -3,5 +3,30 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    {
+      name: "configure-response-headers",
+      configureServer: (server) => {
+        server.middlewares.use((_req, res, next) => {
+          res.setHeader(
+            "Content-Security-Policy",
+            "default-src 'self' 'nonce-exampleexample';",
+          )
+          next()
+        })
+      },
+    },
+    react(),
+  ],
+  build: {
+    sourcemap: true,
+    minify: false,
+  },
+  server: {
+    hmr: {
+      protocol: "ws", // force insecure, since our ssl stuff won't proxy this
+      host: "localhost",
+      port: 53314, // just a random port to avoid conflicts
+    },
+  },
 })
